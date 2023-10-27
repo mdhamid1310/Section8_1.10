c5dad3a416693f4dd7ce9f9434c292137fc57874
mdhamid1310 <john@example.com>
2023-10-27 09:13:17 +0530

    Add project files.

---
+++ Section8_1.10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackLibrary;
+
+namespace Section8_1._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+
+            // Push elements onto the stack
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine("Stack elements:");
+            while (!stack.IsEmpty())
+            {
+                int element = stack.Pop();
+                Console.WriteLine(element);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StackLibrary/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackLibrary
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+}
